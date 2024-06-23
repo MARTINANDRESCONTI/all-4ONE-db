283029b8f14b965d3e3f110160c7e5fc8107aba4
Martin Conti <matin.a.conti2014@gmail.com>
2024-06-22 21:09:49 -0300

    debug1

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const { db } = require('./database/db.js');
+// const { db } = require('./database/db.js');
 
-const itemRoutes = require('./routes/routes.js')
+// const itemRoutes = require('./routes/routes.js')
 
 const app = express();
 
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 3000
 app.use( cors() )
 app.use(express.json())
 app.use(morgan('dev'))
-app.use('/items', itemRoutes)
+// app.use('/items', itemRoutes)
 
 app.get('/', (req, res)=>{
-  res.send('HOla mundo')
+  res.send('Hola mundo')
 })
 
 app.listen(PORT, () =>{
   console.log(`server runing on port:${PORT}`);
-  db.sync({alter:true});
+  // db.sync({alter:true});
 })
